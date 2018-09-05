9d71b9d82b529909285330c3841aa17c48294e56
Vitor Nascimento <vitor.nascimento.jr@gmail.com>
2018-09-04 22:17:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var apib2swagger = module.exports.convertParsed = function(apib) {
     swagger.swagger = '2.0';
     swagger.info = {
         'title': apib.name,
-        'version': apib.name,
+        'version': '1.0.0',
         'description': apib.description
     }
     apib.metadata.forEach(function(meta) {
